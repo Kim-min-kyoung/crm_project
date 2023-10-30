52a703bba7fb5a139d292e9671e04f96f24e2ae9
wtmo <go.youngjin@gmail.com>
2023-10-30 12:09:01 +0900

    mb_nick값 수정

--- adm/member_form_update.php
+++ adm/member_form_update.php
@@ -37,7 +37,7 @@ $mb_zip1 = substr($mb_zip, 0, 3);
 $mb_zip2 = substr($mb_zip, 3);
 
 $mb_email = isset($_POST['mb_email']) ? get_email_address(trim($_POST['mb_email'])) : '';
-$mb_nick = isset($_POST['mb_nick']) ? trim(strip_tags($_POST['mb_name'])) : '';
+$mb_nick = isset($_POST['mb_name']) ? trim(strip_tags($_POST['mb_name'])) : '';
 
 // if ($msg = valid_mb_nick($mb_nick))     alert($msg, "", true, true);
 
